34b0f0c9a0edcf3f829f215c908a1039a071d44a
Chaskiel Grundman <cgrundman@gmail.com>
2024-05-18 14:11:57 -0400

    Clean up storage.py
    Fix formatting
    Update type hints

--- multicred/storage.py
+++ multicred/storage.py
@@ -1,5 +1,5 @@
 from typing import Type
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.exc import NoResultFound, MultipleResultsFound, IntegrityError
 
@@ -8,10 +8,15 @@ from . import credentials
 
 
 class Storage:
+    engine: Engine
+    # https://github.com/sqlalchemy/sqlalchemy/issues/7656
+    # not sure why this had to be a string
+    session: 'sessionmaker[Session]'
+
     def __init__(self, db_uri):
         self.engine = create_engine(db_uri)
         schema.Base.metadata.create_all(self.engine)
-        self.Session = sessionmaker(bind=self.engine)
+        self.session = sessionmaker(bind=self.engine)
 
     # https://stackoverflow.com/a/21146492
     def get_one_or_create(self,
@@ -21,49 +26,55 @@ class Storage:
                           create_method_kwargs=None,
                           **kwargs):
         try:
-            return session.query(model).filter_by(**kwargs).one(), True
+            return session.query(model).filter_by(**kwargs).with_for_update().one(), True
         except NoResultFound:
             kwargs.update(create_method_kwargs or {})
             try:
                 with session.begin_nested():
-                    created = getattr(model, create_method, model)(**kwargs)
+                    created: schema.Base = getattr(
+                        model, create_method, model)(**kwargs)
                     session.add(created)
                 return created, False
             except IntegrityError:
                 return session.query(model).filter_by(**kwargs).one(), True
 
     def import_credentials(self, creds: credentials.Credentials):
-        session = self.Session()
+        session = self.session()
         identity = creds.aws_identity
-        account, _ = self.get_one_or_create(session, schema.AwsAccountStorage, account_id=identity.aws_account_id)
+        account, _ = self.get_one_or_create(
+            session, schema.AwsAccountStorage, account_id=identity.aws_account_id)
         assert isinstance(account, schema.AwsAccountStorage)
         if identity.cred_type == 'role':
             assert isinstance(identity, credentials.AwsRoleIdentity)
-            stored_id, _ = self.get_one_or_create(session, schema.AwsIdentityStorage, aws_account=account,
-                                                 cred_type=identity.cred_type, name=identity.aws_role_name,
-                                                  create_method_kwargs={'arn': identity.aws_identity})
+            stored_id, _ = self.get_one_or_create(
+                session, schema.AwsIdentityStorage, aws_account=account,
+                cred_type=identity.cred_type, name=identity.aws_role_name,
+                create_method_kwargs={'arn': identity.aws_identity})
         elif identity.cred_type == 'user':
             assert isinstance(identity, credentials.AwsUserIdentity)
-            stored_id, _ = self.get_one_or_create(session, schema.AwsIdentityStorage, aws_account=account,
-                                                  cred_type=identity.cred_type, name=identity.aws_user_name,
-                                                  create_method_kwargs={'arn': identity.aws_identity})
+            stored_id, _ = self.get_one_or_create(
+                session, schema.AwsIdentityStorage, aws_account=account,
+                cred_type=identity.cred_type, name=identity.aws_user_name,
+                create_method_kwargs={'arn': identity.aws_identity})
         else:
             raise ValueError('Unknown cred_type')
-        credential = schema.AwsCredentialStorage(aws_identity=stored_id, aws_access_key_id=creds.aws_access_key_id,
-                                                 aws_secret_access_key=creds.aws_secret_access_key,
-                                                 aws_session_token=creds.aws_session_token)
+        credential = schema.AwsCredentialStorage(
+            aws_identity=stored_id, aws_access_key_id=creds.aws_access_key_id,
+            aws_secret_access_key=creds.aws_secret_access_key,
+            aws_session_token=creds.aws_session_token)
         session.add(credential)
         session.commit()
         session.close()
+
     def add_account(self, account_id):
-        session = self.Session()
+        session = self.session()
         account = schema.AwsAccountStorage(account_id=account_id)
         session.add(account)
         session.commit()
         session.close()
 
     def get_account(self, account_id):
-        session = self.Session()
+        session = self.session()
         try:
             account = session.query(schema.AwsAccountStorage).filter_by(
                 account_id=account_id).one()
@@ -73,7 +84,7 @@ class Storage:
         return account
 
     def add_identity(self, account_id, identity: credentials.AwsIdentity):
-        session = self.Session()
+        session = self.session()
         account = session.query(schema.AwsAccountStorage).filter_by(
             account_id=account_id).one()
         if isinstance(identity, credentials.AwsRoleIdentity):
@@ -82,14 +93,15 @@ class Storage:
             name = identity.aws_user_name
         else:
             name = 'unknown'
-        stored_id = schema.AwsIdentityStorage(aws_identity=identity.aws_identity, aws_account_id=account.id,
+        stored_id = schema.AwsIdentityStorage(aws_identity=identity.aws_identity,
+                                              aws_account_id=account.id,
                                               cred_type=identity.cred_type, name=name)
         session.add(stored_id)
         session.commit()
         session.close()
 
     def get_identity(self, account_id, identity):
-        session = self.Session()
+        session = self.session()
         try:
             account = session.query(schema.AwsAccountStorage).filter_by(
                 account_id=account_id).one()
@@ -101,17 +113,19 @@ class Storage:
         return identity
 
     def add_credential(self, identity, credential):
-        session = self.Session()
+        session = self.session()
         identity = session.query(schema.AwsIdentityStorage).filter_by(
             aws_identity=identity).one()
-        credential = schema.AwsCredentialStorage(aws_identity_id=identity.id, aws_access_key_id=credential.aws_access_key_id,
-                                                 aws_secret_access_key=credential.aws_secret_access_key, aws_session_token=credential.aws_session_token)
+        credential = schema.AwsCredentialStorage(
+            aws_identity_id=identity.id, aws_access_key_id=credential.aws_access_key_id,
+            aws_secret_access_key=credential.aws_secret_access_key,
+              aws_session_token=credential.aws_session_token)
         session.add(credential)
         session.commit()
         session.close()
 
     def get_credential(self, identity):
-        session = self.Session()
+        session = self.session()
         try:
             identity = session.query(schema.AwsIdentityStorage).filter_by(
                 aws_identity=identity).one()
