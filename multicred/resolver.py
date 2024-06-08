7ad106512b72126b97313cde9e0f803bb806c3a6
Chaskiel Grundman <cgrundman@gmail.com>
2024-06-08 14:15:59 -0400

    Add role chain resolver
    Allow the resolver to follow a chain of role assumptions from a target
    role name to a credential we have, acquire the credentials, and store
    all the intermediate role credentialss in the storage for later use.

--- multicred/resolver.py
+++ multicred/resolver.py
@@ -1,7 +1,18 @@
+from dataclasses import dataclass
+from typing import Tuple, TYPE_CHECKING
 import boto3
 
 from . import interfaces
+from . import schema
 from . import credentials
+
+if TYPE_CHECKING:
+    from mypy_boto3_sts.client import STSClient
+
+@dataclass(frozen=True)
+class ResolverState:
+    identity: schema.AwsIdentityStorage
+    role_name: str
 
 class DBResolver(interfaces.Resolver):
     _storage: interfaces.Storage
@@ -9,15 +20,51 @@ class DBResolver(interfaces.Resolver):
     def __init__(self, storage: interfaces.Storage):
         self._storage = storage
 
+    def _build_path(self, identity: schema.AwsIdentityStorage) -> Tuple[list[ResolverState], credentials.Credentials | None]:
+        rv = []
+        while True:
+            creds = self._storage.get_identity_credentials(identity)
+            if creds is not None:
+                rv.append(ResolverState(identity=identity, role_name=''))
+                break
+            parent, role_name = self._storage.get_parent_identity(identity)
+            if parent is None:
+                break
+            assert role_name is not None
+            rv.append(ResolverState(identity=identity, role_name=role_name))
+            identity = parent
+        return rv, creds
+
+    def _get_credentials_from_path(self, creds: credentials.Credentials, path: list[ResolverState]) -> credentials.Credentials | None:
+        while path:
+            item = path.pop()
+            role_name = item.role_name
+            identity = item.identity
+            client = boto3.client('sts', **creds.get_boto3_credentials())
+            response = client.assume_role(
+                RoleArn=role_name,
+                RoleSessionName=identity.name)
+            creds = credentials.Credentials(
+                aws_access_key_id=response['Credentials']['AccessKeyId'],
+                aws_secret_access_key=response['Credentials']['SecretAccessKey'],
+                aws_session_token=response['Credentials']['SessionToken'])
+            self._storage.import_credentials(creds)
+        return creds
+
+    def _get_credentials_for_identity(self, identity: schema.AwsIdentityStorage) -> credentials.Credentials | None:
+        rv = self._storage.get_identity_credentials(identity)
+        if rv and rv.is_valid:
+            return rv
+        path, creds = self._build_path(identity)
+        if creds is None:
+            return None
+        return self._get_credentials_from_path(creds, path)
+
     def get_credentials_by_arn(self, arn: str) -> credentials.Credentials | None:
         identity = self._storage.get_identity_by_arn(arn)
         if identity is None:
             return None
-        rv = self._storage.get_identity_credentials(identity)
-        if rv and rv.is_valid:
-            return rv
-        # XXX recurse
-        return None
+        return self._get_credentials_for_identity(identity)
 
     def get_credentials_by_key(self, access_key: str) -> credentials.Credentials | None:
         return self._storage.get_credentials_by_key(access_key)
@@ -27,8 +74,4 @@ class DBResolver(interfaces.Resolver):
         identity = self._storage.get_identity_by_account_and_role_name(account_id, role_name)
         if identity is None:
             return None
-        rv = self._storage.get_identity_credentials(identity)
-        if rv and rv.is_valid:
-            return rv
-        # XXX recurse
-        return None
+        return self._get_credentials_for_identity(identity)
