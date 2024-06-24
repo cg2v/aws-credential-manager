f945efaf85088a1a1cb84e66f27419752d245ef9
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2024-06-24 14:32:48 -0400

    Bump version
    pipx only upgrades an installation when the version changes, so change the
    version to allow the fixed cli to be installed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='multicred',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(exclude=['test*']),
     install_requires=[
         'chardet',
