109d8d3252268cb91545741d409972ba7736b162
Chaskiel Grundman <cgrundman@gmail.com>
2024-07-08 09:09:42 -0400

    Integrate miniver
    Integrate miniver from https://github.com/jbweston/miniver
    No license change required

--- multicred/__init__.py
+++ multicred/__init__.py
@@ -1,6 +1,7 @@
 from .interfaces import Resolver, Storage
 from .dbstorage import DBStorage
 from .resolver import StorageBasedResolver
+from ._version import __version__
 
 def get_resolver(db_uri: str) -> Resolver:
     storage = DBStorage(db_uri)
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 from setuptools import setup, find_packages
 
+# Loads _version.py module without importing the whole package.
+def get_version_and_cmdclass(pkg_path):
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+    spec = spec_from_file_location(
+        'version', os.path.join(pkg_path, '_version.py'),
+    )
+    if spec is None:
+        raise ValueError(f'No version file found in {pkg_path}')
+    if spec.loader is None:
+        raise ValueError(f'No loader found in {spec}')
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.get_cmdclass(pkg_path)
+
+
+version, cmdclass = get_version_and_cmdclass('my_package')
+
 setup(
     name='multicred',
-    version='0.1.4',
+    version=version,
+    cmdclass=cmdclass,
     packages=find_packages(exclude=['test*']),
     install_requires=[
         'chardet',
