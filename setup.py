8f30d3c1cb6e6765fcba7387cb8d8793cf2f24f3
Chaskiel Grundman <cgrundman@gmail.com>
2024-07-08 13:55:13 -0400

    Fix miniver integration
    Refer to our correct package name

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def get_version_and_cmdclass(pkg_path):
     return module.__version__, module.get_cmdclass(pkg_path)
 
 
-version, cmdclass = get_version_and_cmdclass('my_package')
+version, cmdclass = get_version_and_cmdclass('multicred')
 
 setup(
     name='multicred',
