83d3b6255a44b56e18dee5c7e0e726f16557f8c5
Chaskiel Grundman <cgrundman@gmail.com>
2024-07-21 12:50:43 -0400

    Add configparser import/export to credentials
    Add methods to the credentials module to import and export credentials
    and identities via configparser. This will allow the file and redis
    storage classes to share serialization code.

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,4 +1,4 @@
-
+from multicred.credentials import Credentials
 
 def test_role_credentials(role_credentials):
     creds = role_credentials.test_object
@@ -26,3 +26,35 @@ def test_user_credentials(user_credentials):
     assert creds.aws_identity.cred_type.value == 'user'
     assert creds.aws_identity.aws_account_id == '123456789012'
     assert creds.aws_identity.aws_user_name == 'test_user'
+
+def test_credentials_export(role_credentials, user_credentials):
+    config = role_credentials.test_object.put()
+    assert "credentials" in config
+    assert config.get("credentials", "aws_access_key_id") == role_credentials.access_key_id
+    assert config.get("credentials", "aws_secret_access_key") == role_credentials.secret_access_key
+    assert config.get("credentials", "aws_session_token") == role_credentials.session_token
+
+    config = user_credentials.test_object.put()
+    assert "credentials" in config
+    assert config.get("credentials", "aws_access_key_id") == user_credentials.access_key_id
+    assert config.get("credentials", "aws_secret_access_key") == user_credentials.secret_access_key
+    assert not config.has_option("credentials", "aws_session_token")
+    
+def test_credentials_import(role_credentials, user_credentials):
+    data = (
+        '[credentials]',
+        'aws_access_key_id = ' + role_credentials.access_key_id,
+        'aws_secret_access_key = ' + role_credentials.secret_access_key,
+        'aws_session_token = ' + role_credentials.session_token
+    )
+ 
+    imported_creds = Credentials.from_shared_credentials_file(data, profile_name='credentials')
+    assert imported_creds == role_credentials.test_object
+
+    data = (
+        '[credentials]',
+        'aws_access_key_id = ' + user_credentials.access_key_id,
+        'aws_secret_access_key = ' + user_credentials.secret_access_key
+    )
+    imported_creds = Credentials.from_shared_credentials_file(data, profile_name='credentials')
+    assert imported_creds == user_credentials.test_object
