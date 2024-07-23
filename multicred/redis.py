867be80ebb941c9c7ecb4355fe161b9c1f8cafe8
Chaskiel Grundman <cg2v@andrew.cmu.edu>
2024-07-23 14:21:20 -0400

    Refactor redis key management
    Instead of building redis keys ad-hoc, create RedisStorage._build_key
    that processes a list of args into a key. This will make it easier to
    manage keys and avoid errors (missing prefix, etc)
    
    Also rename the :idcredentials:{id} key to :identity:{id}:credentials
    so it's more clear what the structure of the key components is.

--- multicred/redis.py
+++ multicred/redis.py
@@ -6,7 +6,7 @@ from configparser import ConfigParser
 import redis
 
 from . import credentials
-from .base_objects import IdentityHandle, MultiCredError,\
+from .base_objects import IdentityKey, IdentityHandle, MultiCredError,\
       MultiCredBadRequest, MultiCredLinkError
 from .interfaces import Statistics, CredentialInfo
 from .utils import parse_principal
@@ -32,21 +32,29 @@ class RedisStorage:
         config.read_string(data)
         return credentials.get_identity(config)
 
-    def _get_key_from_handle(self, handle: IdentityHandle, /, namespace='identity') -> str:
-        return f'{self._prefix}:{namespace}:{handle.cred_type.value}:'\
-            f'{handle.aws_account_id}:{handle.name}'
+    def _build_key(self, *args) -> str:
+        def transform(arg):
+            if isinstance(arg, (IdentityHandle, IdentityKey)):
+                return ':'.join((arg.cred_type.value, arg.aws_account_id, arg.name))
+            if isinstance(arg, str):
+                return arg
+            raise RedisStorageError(f'Invalid key component type {type(arg)}')
+        return self._prefix + ':' + ':'.join(map(transform, args))
+
+    def _get_key_from_handle(self, handle: IdentityHandle | IdentityKey, /, namespace='identity') -> str:
+        return self._build_key(namespace, handle)
 
     def get_identity_by_arn(self, arn: str) -> IdentityHandle | None:
         try:
-            key = parse_principal(arn)
+            identity = parse_principal(arn)
         except ValueError:
             return None
-        key = f'{self._prefix}:identity:{key.cred_type.value}:{key.aws_account_id}:{key.name}'
+        key = self._get_key_from_handle(identity)
         return self._get_handle_from_key(key)
 
     def get_identity_by_account_and_role_name(self, account_id: str, role_name: str) \
         -> IdentityHandle | None:
-        key = f'{self._prefix}:identity:role:{account_id}:{role_name}'
+        key = self._build_key('identity', 'role', account_id, role_name)
         return self._get_handle_from_key(key)
 
     def get_parent_identity(self, identity: IdentityHandle) \
@@ -112,30 +120,30 @@ class RedisStorage:
                 creds.aws_identity.put().write(buf)
                 data = buf.getvalue()
             self._redis.set(key, data)
-        account_key = f'{self._prefix}:account:{creds.aws_identity.aws_account_id}'
+        account_key = self._build_key('account', creds.aws_identity.aws_account_id)
         self._redis.sadd(account_key, key)
-        all_identities_key = f'{self._prefix}:identities'
+        all_identities_key = self._build_key('identities')
         self._redis.sadd(all_identities_key, key)
-        credkey = f'{self._prefix}:credentials:{creds.aws_access_key_id}'
+        credkey = self._build_key('credentials', creds.aws_access_key_id)
         if self._redis.exists(credkey):
             raise MultiCredBadRequest(f'Credentials already exist for {creds.aws_access_key_id}')
         with StringIO() as buf:
             creds.put().write(buf)
             data = buf.getvalue()
         self._redis.set(credkey, data)
-        id_creds_key = self._get_key_from_handle(creds.aws_identity, namespace='idcredentials')
+        id_creds_key = self._build_key('identity', creds.aws_identity, 'credentials')
         self._redis.zadd(id_creds_key, {creds.aws_access_key_id: datetime.now().timestamp()})
-        cred_id_key = f'{self._prefix}:credentials:{creds.aws_access_key_id}:identity'
+        cred_id_key = self._build_key('credentials', creds.aws_access_key_id, 'identity')
         self._redis.set(cred_id_key, key)
 
     def get_identity_credentials(self, identity: IdentityHandle) \
         -> credentials.Credentials | None:
-        id_creds_key = self._get_key_from_handle(identity, namespace='idcredentials')
+        id_creds_key = self._build_key('identity', identity, 'credentials')
         keys = self._redis.zrevrange(id_creds_key, 0, 0)
         if not keys:
             return None
         key = keys[0]
-        credkey = f'{self._prefix}:credentials:{key}'
+        credkey = self._build_key('credentials', key)
         data = self._redis.get(credkey)
         if not data:
             return None
@@ -144,29 +152,29 @@ class RedisStorage:
                                                                   profile_name='credentials')
 
     def _delete_credentials_by_key(self, access_key: str, purging=False) -> None:
-        credkey = f'{self._prefix}:credentials:{access_key}'
+        credkey = self._build_key('credentials', access_key)
         self._redis.delete(credkey)
-        cred_id_key = f'{self._prefix}:credentials:{access_key}:identity'
+        cred_id_key = self._build_key('credentials', access_key, 'identity')
         identity_key = self._redis.get(cred_id_key)
 
         if identity_key:
             self._redis.delete(cred_id_key)
             if not purging:
-                id_creds_key = identity_key.replace('identity', 'idcredentials')
+                id_creds_key = identity_key + ':credentials'
                 self._redis.zrem(id_creds_key, access_key)
 
     def delete_credentials_by_key(self, access_key: str) -> None:
         self._delete_credentials_by_key(access_key)
 
     def purge_identity_credentials(self, identity: IdentityHandle) -> None:
-        cred_id_key = self._get_key_from_handle(identity, namespace='idcredentials')
+        cred_id_key = self._build_key('identity', identity, 'credentials')
         keys = self._redis.zrange(cred_id_key, 0, -1)
         for key in keys:
             self._delete_credentials_by_key(key, purging=True)
         self._redis.delete(cred_id_key)
 
     def get_credentials_by_key(self, access_key: str) -> credentials.Credentials | None:
-        credkey = f'{self._prefix}:credentials:{access_key}'
+        credkey = self._build_key('credentials', access_key)
         data = self._redis.get(credkey)
         if not data:
             return None
@@ -180,30 +188,33 @@ class RedisStorage:
         account_count = 0
         max_cred_count = 0
         for key in self._redis.scan_iter(f'{self._prefix}:identity:*'):
+            if key.count(':') != 4:
+                continue
             identity_count += 1
             if key.startswith(f'{self._prefix}:identity:role:'):
                 role_count += 1
         for key in self._redis.scan_iter(f'{self._prefix}:account:*'):
             account_count += 1
         for key in self._redis.scan_iter(f'{self._prefix}:credentials:*'):
-            if not key.endswith(':identity'):
+            if key.count(':') != 3:
                 credential_count += 1
-        for key in self._redis.scan_iter(f'{self._prefix}:idcredentials:*'):
+        for key in self._redis.scan_iter(f'{self._prefix}:identity:*:*:*:credentials'):
             max_cred_count = max(max_cred_count, self._redis.zcard(key))
         return Statistics(identity_count, credential_count, role_count,
                           account_count, max_cred_count)
 
     def list_identities(self) -> Iterator[IdentityHandle]:
-        for key in self._redis.sort(f'{self._prefix}:identities', alpha=True):
+        ids_key = self._build_key('identities')
+        for key in self._redis.sort(ids_key, alpha=True):
             data = self._get_handle_from_key(key)
             if data:
                 yield data
 
     def list_identity_credentials(self, identity: IdentityHandle) -> Iterator[CredentialInfo]:
-        id_creds_key = self._get_key_from_handle(identity, namespace='idcredentials')
+        id_creds_key = self._build_key('identity', identity, 'credentials')
         keys = self._redis.zrange(id_creds_key, 0, -1, withscores=True, desc=True)
         for key in keys:
-            credkey = f'{self._prefix}:credentials:{key[0]}'
+            credkey = self._build_key('credentials', key[0])
             data = self._redis.get(credkey)
             if not data:
                 continue
